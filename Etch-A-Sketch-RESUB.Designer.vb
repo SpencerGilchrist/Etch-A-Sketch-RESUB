6018e5afb4a298cf9bd0d08ef6164524a23b1f17
Gilcspen@gmail.com <Gilcspen@gmail.com>
2021-04-08 08:34:59 -0600

    Add project files.

---
+++ Etch-A-Sketch-RESUB.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ColorDialog = New System.Windows.Forms.ColorDialog()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ColorButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.FillButton = New System.Windows.Forms.Button()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SelectColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 33)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 49)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(769, 351)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(54, 29)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SelectColorToolStripMenuItem, Me.ClearToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(58, 29)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(65, 29)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(605, 406)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(175, 38)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ColorButton
+        '
+        Me.ColorButton.Location = New System.Drawing.Point(424, 406)
+        Me.ColorButton.Name = "ColorButton"
+        Me.ColorButton.Size = New System.Drawing.Size(175, 38)
+        Me.ColorButton.TabIndex = 3
+        Me.ColorButton.Text = "Select Color"
+        Me.ColorButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(193, 406)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(175, 38)
+        Me.ClearButton.TabIndex = 4
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'FillButton
+        '
+        Me.FillButton.Location = New System.Drawing.Point(12, 406)
+        Me.FillButton.Name = "FillButton"
+        Me.FillButton.Size = New System.Drawing.Size(175, 38)
+        Me.FillButton.TabIndex = 5
+        Me.FillButton.Text = "Fill Color"
+        Me.FillButton.UseVisualStyleBackColor = True
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'SelectColorToolStripMenuItem
+        '
+        Me.SelectColorToolStripMenuItem.Name = "SelectColorToolStripMenuItem"
+        Me.SelectColorToolStripMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.SelectColorToolStripMenuItem.Text = "Select Color"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.FillButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.ColorButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Etch-A-Sketch-RESUB"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SelectColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ColorDialog As ColorDialog
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ColorButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents FillButton As Button
+End Class
---
+++ Etch-A-Sketch-RESUB.vb
@@ -0,0 +1,82 @@
+﻿'Spencer Gilchrist
+'RCET 0265
+'Etch-A-Sketch
+'3/24/21
+Option Explicit On
+Option Strict On
+
+Public Class Form1
+    'Global feilds
+    Dim MainPen As New Pen(Color.Black)
+    Dim G As Graphics
+    Sub MouseDraw(x1 As Integer, y1 As Integer, x2 As Integer, y2 As Integer)
+        Dim G As Graphics = PictureBox1.CreateGraphics
+        'This handels the drawing of the lines
+        G.DrawLine(Me.MainPen, x1, y1, x2, y2) 'Put me in if you want to use global
+        'pen.Dispose()
+        G.Dispose()
+    End Sub
+    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown, PictureBox1.MouseMove
+        Static Oldx As Integer
+        Static Oldy As Integer
+        'This is the main drawing program and allows the user to draw...
+        'Using the left mouse button click
+        Dim MyPoint As Point
+        MyPoint.Y = 0
+        MyPoint.X = 0
+
+        Me.Text = $"({e.X},{e.Y}) Button: {e.Button.ToString} Color: {Me.MainPen.Color.ToString}"
+        Select Case e.Button.ToString
+            Case "Left"
+                MouseDraw(Oldx, Oldy, e.X, e.Y)
+            Case "Right"
+                'This show the user what they can use when they click the set button on the mouse
+            Case "Middle"
+                ColorDialog.ShowDialog()
+                Me.MainPen.Color = ColorDialog.Color
+            Case "None"
+
+            Case Else
+                MsgBox("Problems are a foot.")
+        End Select
+        Oldx = e.X
+        Oldy = e.Y
+    End Sub
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click, ClearToolStripMenuItem.Click
+        'Clear button
+        Shake() 'this shakes the screen
+        PictureBox1.BackColor = Color.White
+        PictureBox1.Refresh()
+
+    End Sub
+    Sub Shake()
+        Dim MoveAmount = 50
+        'This shakes the screen when the clear button is pressed
+        For i = 1 To 10
+            Me.Top += MoveAmount
+            Me.Left += MoveAmount
+            System.Threading.Thread.Sleep(100)
+            MoveAmount *= -1
+        Next
+
+    End Sub
+    Private Sub ChangeColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SelectColorToolStripMenuItem.Click, ColorButton.Click
+        'Tool strip menu change color
+        ColorDialog.ShowDialog()
+        Me.MainPen.Color = ColorDialog.Color
+    End Sub
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        MsgBox("This Program is a kind of paint clone, created for Tim Rossiter." & vbNewLine &
+               "This program was created for Visual  Basic on 3/24/21.")
+    End Sub
+
+    Private Sub FillButton_Click(sender As Object, e As EventArgs) Handles FillButton.Click
+        ColorDialog.ShowDialog() 'this changes the color of the background when the user selects a color
+        PictureBox1.BackColor = ColorDialog.Color
+    End Sub
+
+End Class
